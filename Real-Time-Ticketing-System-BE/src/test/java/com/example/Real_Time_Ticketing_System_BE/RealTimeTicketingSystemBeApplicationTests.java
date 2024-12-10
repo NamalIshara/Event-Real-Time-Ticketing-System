bf516fc4842d54a70e0e0aa03d19c6811440d996
namal.i <namal.i@informaticsint.com>
2024-12-10 21:37:12 +0530

    initial commit

---
+++ Real-Time-Ticketing-System-BE/src/test/java/com/example/Real_Time_Ticketing_System_BE/RealTimeTicketingSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Real_Time_Ticketing_System_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTicketingSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
